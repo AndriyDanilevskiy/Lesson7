bcd3dfcf0efc0850afebdb163aa04338328f47d6
Danilevskyy Andriy <andriy_danilevskyy@iownit.us>
2022-09-10 18:33:43 +0300

    updates

--- src/ua/andriy/danilevskyy/hillel/guessWord/Game.java
+++ src/ua/andriy/danilevskyy/hillel/guessWord/Game.java
@@ -14,13 +14,12 @@ public class Game {
         for (int i = 1; i <= hiddenWord.length(); i++) {
             input = scanner.nextLine();
             if (!input.equals(hiddenWord)) {
-                StringBuilder openedWord = new StringBuilder(hiddenWord);
-                openedWord.delete(i, openedWord.length());
+                StringBuilder openedWord = new StringBuilder(hiddenWord).delete(i, hiddenWord.length());
                 if (i == hiddenWord.length()) {
                     System.out.println("The word is " + openedWord);
                 } else {
                     System.out.println("Wrong, try again!");
-                    System.out.println("The word is " + openedWord + "#############");
+                    System.out.println("The word is " + openedWord.append("###############").substring(0,15));
                 }
             } else {
                 isWordFound = true;
